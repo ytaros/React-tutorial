09ef51e22bdae1ef1dc171814902b993030dac91
ytaros <hoo.miii0616@gmail.com>
2023-12-17 23:44:57 +0900

    💫 第４回React勉強会

--- 06_control_and_form/src/010_list_components/start/Example.js
+++ 06_control_and_form/src/010_list_components/start/Example.js
@@ -1,13 +1,28 @@
-
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
+  // POINT for文でJSXの配列を作成.他の文では代入できない。
+  const animalList = [];
+  for (const animal of animals) {
+    animalList.push(<li>{animal}</li>);
+  }
 
+  // POINT map関数でJSXの配列を作成。式は代入できる。
+  const helloAnimals = animals.map((animal) => {
+    return <li>Hello {animal}</li>;
+  });
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-        
+        {/* <li>{animals[0]}</li>
+        <li>{animals[1]}</li>
+        <li>{animals[2]}</li> */}
+        {/* {animalList}
+        {helloAnimals} */}
+        {animals.map((animal) => (
+          <li>Hello, {animal}</li>
+        ))}
       </ul>
     </>
   );
--- 06_control_and_form/src/020_list_and_key/start/Example.js
+++ 06_control_and_form/src/020_list_and_key/start/Example.js
@@ -1,4 +1,3 @@
-
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
@@ -21,10 +20,26 @@ const Example = () => {
         {helloAnimals} */}
         {/* {animalList} */}
 
-        {animals.map((animal) => <li>Hello, {animal}</li>)}
+        {animals.map((animal) => (
+          <li key={animal}>Hello, {animal}</li>
+        ))}
       </ul>
     </>
   );
 };
 
 export default Example;
+
+// ReactはReact要素ツリーに変更があった場合に、差分を取得して変更しないといけないものを
+// ブラウザのDOMに対して行うようになっている。React要素ツリーと、レンダリングされた最新のReact要素ツリーを見比べて、
+// 変更分（差分だけ）をブラウザに反映させる。差分検出。
+
+// React要素ツリーの一番最後に変更（追加）があった場合は、（Real)DOMの下に追加。
+// 先頭に挿入があった場合は、すべての子のReact要素を変更してしまい、すべてのReal DOMを洗いかえる。
+// 子要素をすべて削除して、入れ替えることを洗い替えるという。
+
+// Keyを持たせていれば、Reactはどの要素が変更。追加、削除されたかを識別できるようになるため、差分のみを更新することが可能になる。
+
+// ・Keyは一意の値を設定。
+// ・キーに設定した値は変更しない。
+// ・配列のインデックスはなるべく、使わない。
