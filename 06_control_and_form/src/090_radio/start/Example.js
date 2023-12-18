541fe2a2c97c16e09b1970cc775863803fb3de40
ytaros <hoo.miii0616@gmail.com>
2023-12-18 12:30:44 +0900

    🔊 第４回React勉強会

--- 06_control_and_form/src/090_radio/start/Example.js
+++ 06_control_and_form/src/090_radio/start/Example.js
@@ -1,10 +1,47 @@
+import { useState } from "react";
+
+// POINT ラジオボタンの実装
 const Example = () => {
+  const [fruit, setFruit] = useState("Apple");
+  const onChange = (e) => setFruit(e.target.value);
+
+  const RADIO_COLLECTION = ["Apple", "Banana", "Cherry"];
+
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <>
+      {RADIO_COLLECTION.map((value) => {
+        return (
+          <label key={value}>
+            <input
+              type="radio"
+              value={value}
+              checked={fruit === value}
+              onChange={onChange}
+            />
+            {value}
+          </label>
+        );
+      })}
+      {/* <label>
+        <input
+          type="radio"
+          value="Banana"
+          checked={fruit === "Banana"}
+          onChange={onChange}
+        />
+        Banana
+      </label>
+      <label>
+        <input
+          type="radio"
+          value="Cherry"
+          checked={fruit === "Cherry"}
+          onChange={onChange}
+        />
+        Cherry
+      </label> */}
+      <h3>私は{fruit}がたべたい</h3>
+    </>
   );
 };
 
